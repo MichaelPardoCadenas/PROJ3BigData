cc73b0d43187881b27f3eb8c5bb117835a770e90
MichaelPardoCadenas <145122584+MichaelPardoCadenas@users.noreply.github.com>
2024-05-11 16:07:48 +0200

    Add files via upload

--- PROJ3/app.py
+++ PROJ3/app.py
@@ -3,10 +3,10 @@ from requests import post
 import json
 from requests.auth import HTTPBasicAuth
 
+
 def guardar_en_json(data, nombre_archivo):
     with open(nombre_archivo, 'w') as archivo:
         json.dump(data, archivo)
-
 
 
 # CLIENTID = "w8cmra3o6xppg91h5dbqdl1j6pa0h1"
@@ -16,23 +16,85 @@ def guardar_en_json(data, nombre_archivo):
 # "expires_in": 5177967,
 # "token_type": "bearer"
 # }
-
 headers = {
     'Client-ID': 'w8cmra3o6xppg91h5dbqdl1j6pa0h1',
     'Authorization': 'Bearer a70d6z3oa5n5o2mharygi7amnrl285',
 }
 
-data = 'fields name,url,summary,cover,genres,total_rating,release_dates,age_ratings; where rating_count > 80; limit 500;'
+gameData = 'fields name,url,summary,cover,genres,total_rating,release_dates; where total_rating_count > 100; limit 100;'
 
-response = post('https://api.igdb.com/v4/games', headers=headers, data=data)
-with open('games.json', 'w') as file:
-        json.dump(response.json(), file)
+responseData = post('https://api.igdb.com/v4/games',
+                    headers=headers, data=gameData)
+with open('static/json/games.json', 'w') as file:
+    json.dump(responseData.json(), file)
+
+def GameItems():
+    # Leer JSON games
+    with open('static/json/games.json', 'r') as file:
+        games = json.load(file)
+
+    # Obtener todas las fechas de lanzamiento
+    for game in games:
+        # Tomar solo el primer ID de release_dates
+        gameID = game['id']
+        cover = game['cover']
+        genres = game['genres']
+        name = game['name']
+        summary = game['summary']
+        rating = game['total_rating']
+        url = game['url']
+        releaseDate = game['release_dates'][0]
+        
+        # Consultar la API para obtener la fecha de lanzamiento en formato humano
+        gameDates = f'fields game,human; where id = {releaseDate}; limit 1;'
+        responseDates = post('https://api.igdb.com/v4/release_dates',
+                            headers=headers, data=gameDates)
+        
+        # Obtener la fecha de lanzamiento en formato humano
+        date = responseDates.json()[0]['human']
+        
+        # Asignar la fecha de lanzamiento al juego
+        game['release_dates'] = date
+        # Consultar la API para obtener la cubierta del juego
+        gameCover = f'fields url, game, image_id; where id = {cover} ; limit 1;'
+        responseCover = post('https://api.igdb.com/v4/covers',
+                            headers=headers, data=gameCover)
+        # Obtener la url de la cover
+        gameCover = responseCover.json()[0]['url']
+        gameCoverID = responseCover.json()[0]['image_id']
+        gameCover = f"https://images.igdb.com/igdb/image/upload/t_1080p/{gameCoverID}.jpg"
+        
+        # Asignar la cover
+        game['cover'] = gameCover
+
+        # Consultar la API para obtener el genero
+        genresList = []
+        if len(genres) > 1:
+            for i in range(0,2):
+                gameGenre = f'fields name; where id = {genres[i]} ; limit 1;'
+                responseGenre = post('https://api.igdb.com/v4/genres',
+                                    headers=headers, data=gameGenre)
+                gameGenre = responseGenre.json()[0]['name']
+                genresList.append(gameGenre)
+    
+        # Asignar el genero
+        game['genres'] = genresList
+
+    # Escribir los juegos modificados en el archivo JSON original
+    with open('static/json/games.json', 'w') as file:
+        json.dump(games, file)
+
+GameItems()
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home1.html")
+
+# @app.route("/juego")
+# def juego():
+#     return render_template("juego.html")
 
 
-# app = Flask(__name__)
-
-# @app.route("/")
-# def home():
-#     return render_template("home.html")
-
-# app.run(host='localhost', port=5001, debug=True)
+app.run(host='localhost', port=5001, debug=True)
